abb6046ee1d68c06100183d23a7c0cac1a2942ed
Flo <wengelef@gmail.com>
2018-09-08 00:01:06 +0200

    feature (trends for location): display trends in a recyclerview

--- app/src/main/java/test/wengelef/twitterfeed/MainActivity.kt
+++ app/src/main/java/test/wengelef/twitterfeed/MainActivity.kt
@@ -6,12 +6,25 @@ import test.wengelef.twitterfeed.locationtrends.view.LocationTrendsFragment
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val LOCATION_FRAGMENT_TAG = "LocationTrendsFragment"
+    }
+    private lateinit var locationFragment: LocationTrendsFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, LocationTrendsFragment())
+        locationFragment = if (savedInstanceState == null) {
+            LocationTrendsFragment()
+        } else {
+            supportFragmentManager.findFragmentByTag(LOCATION_FRAGMENT_TAG) as LocationTrendsFragment
+        }
+
+
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, locationFragment, LOCATION_FRAGMENT_TAG)
                 .commit()
     }
 }
