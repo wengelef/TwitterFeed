ed149581b2bd0567ca1c9d5bec158cff911b3a73
Flo <wengelef@gmail.com>
2018-09-08 00:05:59 +0200

    --rm tokens

--- app/src/main/java/test/wengelef/twitterfeed/locationtrends/Auth.kt
+++ app/src/main/java/test/wengelef/twitterfeed/locationtrends/Auth.kt
@@ -1,6 +1,6 @@
 package test.wengelef.twitterfeed.locationtrends
 
 object Auth {
-    const val clientId = "lRkenkvnlfXbpK1C4PRKoNTFv"
-    const val clientSecret = "1Pgkxe8FJRk3rH1DoMLJp1cbUlpBgSv2ezxhYhMPDDTs6nzh83"
+    const val clientId = "<insert client id>"
+    const val clientSecret = "<insert client secret>"
 }
